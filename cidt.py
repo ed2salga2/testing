0d63c45918730390aaa367cd82a9bee4c0b2358c
ed2salga2 <126299937+ed2salga2@users.noreply.github.com>
2023-02-26 03:26:24 -0600

    Update cidt.py

--- cidt.py
+++ cidt.py
@@ -2,71 +2,51 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from functools import reduce
-
-import pandas as pd
-
+ 
 def extract_tables(csv):
     tables = {}
     table_idx = 0
+    
     while table_idx < len(csv.index):
         # Look for table name in first column
         if not pd.isna(csv.loc[table_idx, csv.columns[0]]):
             table_name = csv.loc[table_idx, csv.columns[0]]
             data_start_idx = table_idx + 2
-
+            
             # Determine table dimensions
-            col_start_idx = 1
-            while pd.isna(csv.loc[table_idx + 1, csv.columns[col_start_idx]]):
-                col_start_idx += 1
-            col_end_idx = col_start_idx
-            while col_end_idx < csv.shape[1] and not pd.isna(csv.loc[table_idx + 1, csv.columns[col_end_idx]]):
-                col_end_idx += 1
-            num_cols = col_end_idx - col_start_idx
-
-            num_rows = 0
-            while data_start_idx + num_rows < len(csv.index) and not pd.isna(csv.loc[data_start_idx + num_rows, csv.columns[0]]):
+            num_cols = 0
+            for i in range(3):
+                if pd.isna(csv.loc[table_idx, csv.columns[i]]):
+                    break
+                num_cols += 1
+                
+            num_rows = 1
+            while pd.isna(csv.loc[table_idx + num_rows, csv.columns[0]]):
                 num_rows += 1
-
+            
             # Extract table data
-            table_data = csv.iloc[data_start_idx:data_start_idx + num_rows, col_start_idx:col_end_idx]
+            table_data = csv.iloc[data_start_idx:data_start_idx + num_rows - 1, :num_cols]
             table_data = table_data.fillna('')
             table_data = table_data.set_index(table_data.columns[0])
             table_data.index.name = None
             table_data.columns.name = None
             table_data = table_data.apply(pd.to_numeric, errors='coerce')
             tables[table_name] = table_data
-
+            
             # Extract parent/child header information
             headers = {}
-            if isinstance(table_data.columns, pd.MultiIndex):
-                for level in range(table_data.columns.nlevels):
-                    header_level = [h for h in table_data.columns.levels[level] if h]
-                    if header_level:
-                        headers[header_level[0]] = header_level[1:]
-            else:
-                headers[table_data.columns[0]] = list(table_data.columns)[1:]
-
+            for i in range(1, num_cols):
+                header_level = [h for h in table_data.columns.levels[i] if h]
+                headers[header_level[0]] = header_level[1:]
+            
             tables[table_name + '_headers'] = headers
-
+            
             # Move to next table
-            table_idx = data_start_idx + num_rows
+            table_idx += num_rows
         else:
             table_idx += 1
 
     return tables
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def generate_plot(table_data, title):
