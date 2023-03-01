d6ed077a5cfd5d8fbea24c10614a9a1997317fae
ed2salga2 <126299937+ed2salga2@users.noreply.github.com>
2023-02-28 20:49:07 -0600

    opp

--- cidt.py
+++ cidt.py
@@ -1,198 +1,142 @@
-# Import the required libraries
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from io import StringIO
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import os
 
-# Define the get_table_headers function
-import pandas as pd
-import re
+# Function to read .sav file with pd.read_spss()
+def read_sav_file(file_path):
+    df = pd.read_spss(file_path)
+    return df
 
-from io import BytesIO
-from savReaderWriter import SavReader
+# Function to create crosstab and plot it
+def create_crosstab(df, col_name, row_name, chart_type, chart_args):
+    crosstab_df = pd.crosstab(df[row_name], df[col_name])
+    if chart_type == 'bar':
+        fig = px.bar(crosstab_df)
+    elif chart_type == 'line':
+        fig = px.line(crosstab_df)
+    elif chart_type == 'scatter':
+        fig = px.scatter(crosstab_df)
+    else:
+        fig = go.Figure()
+    fig.update_layout(**chart_args)
+    st.plotly_chart(fig)
 
-def extract_tables(csv):
-    df = pd.read_spss(csv)
-
-    # find empty cells in dataframe
-    empty_cells = df.isna()
-
-    tables = {}
-    current_table = None
-
-    for i, row in df.iterrows():
-        # Check if row is empty
-        if row.isnull().all():
-            continue
-
-        # Check if row is the start of a new table
-        if empty_cells.loc[i, 0]:
-            table_name = row[0]
-            tables[table_name] = {}
-            current_table = table_name
-            tables[table_name]['header_rows'] = []
-
-            # find end of table row
-            for j, value in row.iteritems():
-                if pd.notna(value):
-                    end_row = j
-                    break
-
-            # find row indexes
-            tables[table_name]['row_indexes'] = [x for x in range(i+1, df.shape[0]-1) if not empty_cells.loc[x, 0]]
-
-            # find header rows
-            header_rows = []
-            for k in range(i+1, df.shape[0]-1):
-                if empty_cells.loc[k, 3]:
-                    header_rows.append(k)
-            tables[table_name]['header_rows'] = header_rows
-
-            # initialize table data as empty dataframe
-            table_data = pd.DataFrame(columns=df.loc[i:end_row-1, 3:end_row].iloc[0])
-
-            # fill table data with values
-            for k in range(i+2, end_row):
-                table_data.loc[k-1] = df.loc[k, 3:end_row]
-
-            # set row indexes
-            table_data.index = tables[table_name]['row_indexes']
-
-            # set table data in tables dictionary
-            tables[table_name]['data'] = table_data
-
-            # set header levels
-            header_levels = {}
-            parent_stack = []
-            for header_row in header_rows:
-                row = df.loc[header_row, 3:end_row]
-                row_levels = []
-                for cell in row:
-                    if pd.notna(cell):
-                        level = len(parent_stack)
-                        if level not in header_levels:
-                            header_levels[level] = []
-                        if level == 0:
-                            parent_stack = [cell]
-                        else:
-                            if level-1 < len(parent_stack):
-                                parent_stack = parent_stack[:level-1]
-                            parent_stack.append(cell)
-                        row_levels.append(level)
-                    else:
-                        row_levels.append(np.nan)
-                header_levels[len(parent_stack)] = list(set(row_levels))
-            tables[table_name]['header_levels'] = header_levels
-
-    return tables
-
-
-
-               
-
-    
-
-# Define the plot_table function
-def plot_table_data(tables, table_name):
-    # Check if the selected table is in the tables dictionary
-    if table_name not in tables:
-        st.write('Table not found')
-        return
-
-    # Get the headers for the selected table
-    headers = [header[0] for header in get_table_headers({table_name: tables[table_name]})[table_name]]
-
-    # Create a dictionary to store the filter options
-    filter_options = {}
-
-    # Loop through the headers and create a filter dropdown for each column
-    for header in headers:
-        unique_values = sorted(list(set(tables[table_name][header].tolist())))
-        filter_options[header] = st.selectbox(f'Select {header}', ['All'] + unique_values)
-
-    # Create a list of available plot types
-    plot_types = ['line', 'bar', 'scatter', 'hist']
-
-    # Create a dropdown to select the plot type
-    plot_type = st.selectbox('Select plot type', plot_types)
-
-    # Create a list of tuples representing the columns to plot
-    plot_columns = []
-    for header in plot_options['Headers']:
-        if header in tables[table_name].columns:
-            plot_columns.append((header, 'column'))
-        elif header in tables[table_name].index:
-            plot_columns.append((header, 'index'))
-
-    # Check if there are any columns to plot
-    if len(plot_columns) == 0:
-        st.write('No columns selected for plotting')
-        return
-
-    # Create a dictionary to store the plot options
-    plot_options = {
-        'Headers': st.multiselect('Select columns to plot', options=headers),
-        'Title': st.text_input('Enter the plot title', value=table_name),
-        'X Axis Label': st.text_input('Enter the X axis label', value=headers[0]),
-        'Y Axis Label': st.text_input('Enter the Y axis label', value='Count'),
-        'Color': st.color_picker('Select a plot color', value='#1f77b4')
+# Function to save plot as png and update job JSON
+def save_plot_and_update_job(job, df, col_name, row_name, chart_type, chart_args):
+    plot_name = f"{col_name}.png"
+    plot_path = os.path.join(os.path.dirname(df), plot_name)
+    create_crosstab(df, col_name, row_name, chart_type, chart_args)
+    fig = go.Figure()
+    fig.write_image(plot_path)
+    plot_params = {
+        "name": f"#{col_name}",
+        "filter_params": {
+            "col_name": col_name,
+            "row_name": row_name,
+            "chart_type": chart_type,
+            "chart_args": chart_args
+        }
     }
+    job['tables'].append(plot_params)
+    return job
 
-    # Create a button to plot the selected options
-    plot_button = st.button('Plot')
+# Function to create job JSON and save it
+def create_job(job_name, job):
+    job_path = os.path.join(os.getcwd(), "jobs", f"{job_name}.json")
+    with open(job_path, "w") as outfile:
+        json.dump(job, outfile)
+    st.success("Job created successfully!")
 
-    # If the plot button is clicked
-    if plot_button:
-        # Filter the table based on the selected filter options
-        filter_columns = {}
-        for key, value in filter_options.items():
-            if value != 'All':
-                filter_columns[key] = value
-        filtered_table = tables.get(table_name, pd.DataFrame()).copy().reset_index(drop=True)
-        for column, value in filter_columns.items():
-            filtered_table = filtered_table[filtered_table[column] == value]
+# Function to generate the report HTML and close the current job
+def finish_job(job_name, job):
+    job_path = os.path.join(os.getcwd(), "jobs", f"{job_name}.json")
+    with open(job_path, "w") as outfile:
+        json.dump(job, outfile)
+    report_path = os.path.join(os.path.dirname(job_path), f"{job_name}.html")
+    report_content = "<html><head></head><body>"
+    for table in job['tables']:
+        col_name = table['filter_params']['col_name']
+        plot_name = f"{col_name}.png"
+        plot_path = os.path.join(os.path.dirname(job_path), plot_name)
+        report_content += f"<h3>{table['name']}</h3>"
+        report_content += f"<img src='{plot_path}'><br>"
+    report_content += "</body></html>"
+    with open(report_path, "w") as outfile:
+        outfile.write(report_content)
+    st.success("Job finished and report generated!")
+    return {"name": "", "tables": []}
 
-        # If the filtered table is not empty
-        if not filtered_table.empty:
-            # Plot the table based on the selected plot type and plot options
-            plot_table(filtered_table, plot_type, filter_columns, plot_options)
-        else:
-            # Display a message if the filtered table is empty
-            st.write('No data')
-
-# Define the main function
+# App code
 def main():
-    # Set the app title and page icon
-    st.set_page_config(page_title='Crosstab Plotter', page_icon=':chart_with_upwards_trend:')
+    # Initialize app
+    st.set_page_config(page_title="Crosstab Plotter", page_icon="📊")
+    st.title("Crosstab Plotter")
+    job = {"name": "", "tables": []}
 
-    # Set the app header and subheader
-    st.header('Crosstab Plotter')
-    st.subheader('Upload a CSV file to get started!')
+    # Upload file and read sav file with pd.read_spss()
+    file_uploaded = st.file_uploader("Upload a .sav file", type="sav")
+    if file_uploaded is not None:
+        df = read_sav_file(file_uploaded)
 
-    # Create a file uploader widget and prompt the user to upload a CSV file
-    csv_file = st.file_uploader('Upload a CSV file', type='csv')
+        # Run job
+        if st.button("Run Job"):
+            # TODO: Implement code to run all plots based on uploaded file and previously saved JOB JSON files
+            pass
 
-    # If the user has uploaded a file
-    if csv_file is not None:
-        # Extract the tables from the CSV file
-        tables = extract_tables(csv_file.getvalue())
+        # Display LIVE VISUALIZATION PREVIEW based on the crosstab arguments inputted by user
+        st.subheader("Live Visualization Preview")
 
-        # If there are no tables in the CSV file
-        if not tables:
-            st.write('No tables found in the CSV file')
-            return
+        # Select categorical column for row indexes
+        cat_cols = [col for col in df.columns if col.startswith("P")]
+        selected_col = st.selectbox("Select a categorical column for row indexes", cat_cols)
 
-        # Create a dropdown menu to select the table to plot
-        table_name = st.selectbox('Select a table to plot', options=list(tables.keys()))
+        # Input amount of elements for column array
+        amount_elements = len(df.columns) - df.columns.get_loc(selected_col) - 1
+        input_elements = st.number_input(f"Input the amount of elements (up to {amount_elements}) for the column array", min_value=1, max_value=amount_elements, value=1)
 
-        # Call the plot_table_data function to plot the table
-        plot_table_data(tables, table_name)
+        # Define order of categorical columns for input column array
+        input_cols = []
+        for i in range(input_elements):
+            input_col = st.selectbox(f"Select column {i+1} for the input column array", cat_cols)
+            input_cols.append(input_col)
 
-    # Set the app footer
-    st.text('Created by Zompopa Solutions with love for coding')
+        # Customize live preview with plotly arguments
+        chart_type = st.selectbox("Select chart type", ["bar", "line", "scatter"])
+        chart_args = {}
+        chart_args['title'] = st.text_input("Title", value=f"{selected_col}")
+        chart_args['xaxis_title'] = st.text_input("X-axis Title", value=f"{input_cols}")
+        chart_args['yaxis_title'] = st.text_input("Y-axis Title", value="Count")
+        chart_args['legend_title'] = st.text_input("Legend Title", value=f"{selected_col}")
+        chart_args['color_discrete_sequence'] = px.colors.qualitative.Dark24
+        chart_args['template'] = "simple_white"
+        chart_args['showlegend'] = True
+        chart_args['barmode'] = 'stack'
 
-if __name__ == '__main__':
+        # Generate plot and save it as png
+        if st.button("Generate"):
+            job_append = st.checkbox("Append parameters to JOB?")
+            if job_append:
+                job = save_plot_and_update_job(job, df, selected_col, input_cols, chart_type, chart_args)
+            else:
+                job = {"name": "", "tables": []}
+                job = save_plot_and_update_job(job, df, selected_col, input_cols, chart_type, chart_args)
+            st.success(f"Plot {selected_col} generated and saved successfully!")
+            plot_path = os.path.join(os.path.dirname(file_uploaded.name), f"{selected_col}.png")
+            st.image(plot_path)
+
+        # Finish job and generate report
+        if st.button("Finish Job"):
+            job_name = st.text_input("Enter job name")
+            job = finish_job(job_name, job)
+
+    # Create job
+    if st.button("Create Job"):
+        job_name = st.text_input("Enter job name")
+        create_job(job_name, job)
+
+if __name__ == "__main__":
     main()
-
-
 
